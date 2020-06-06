eea3be6cdb556bec9b248027a9314852998373c4
Fernando Gurgel <fag.gurgel@gmail.com>
2020-06-06 18:02:39 -0400

    route done delete, create

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,39 @@
-class Transaction {
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
+export default class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
-  type: 'income' | 'outcome';
-
+  @Column('float')
   value: number;
 
+  @Column('varchar')
+  type: 'income' | 'outcome';
+
+  @Column('varchar')
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
-
-export default Transaction;
